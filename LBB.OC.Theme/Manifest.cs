a52ca461e61ee025ce2ccbbf6b8fed97a2e14746
ErnieBernie10 <arneboedt1@gmail.com>
2025-09-02 09:13:01 +0200

    Move things around

--- LBB.OC.Theme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "LBB.OC.Theme",
-    Author = "The Orchard Core Team",
-    Website = "https://orchardcore.net",
-    Version = "0.0.1",
-    Description = "LBB.OC.Theme"
-)]
